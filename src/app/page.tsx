6492d09742eb17ff02f9fcbdfedd32e0c2d23faa
robertrahardja <robertrahardja@gmail.com>
2024-10-21 11:43:56 +0800

    test second sentence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Home() {
   const data = process.env.TEST_VAR;
   console.log(data);
@@ -7,10 +6,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-        This is Linode One
-        Testing env
-        this is {data}
+        This is Linode One Testing env this is {data}
+        <section>Hello second sentence</section>
       </main>
     </div>
   );
